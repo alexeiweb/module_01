c1fbd82363b21b8463b76d5bfc89e5b2befb63dd
Alexei Web <alexeiiweb@gmail.com>
2021-12-17 10:00:41 +0300

    init

---
+++ Lesson_01/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const product = 'hat';
+const productCount = 3;
+const productCategory = 'cloths';
+const productPrice = 1250;
+
+console.log('product: ',product);
+
+console.log(productCount * productPrice);
+
+
+
