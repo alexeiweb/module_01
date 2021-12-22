7592c1f56ee07d7598b2dbdb3cd5d5ae00f92ac9
Alexei Web <alexeiiweb@gmail.com>
2021-12-22 11:27:09 +0300

    Lesson 3-1

--- einstein.js
+++ einstein.js
@@ -1,4 +1,2 @@
 const myWeight = 90;
 const speedLight = 3e8;
-
-console.log('Энергия тела:', myWeight * (speedLight ** 2)/1e18 + ' ЭДж');
--- main.js
+++ main.js
@@ -4,39 +4,28 @@ const product = 'Шляпа';
 const productCount = 3;
 const productCategory = 'Одежда';
 const productPrice = 1250;
-
-console.log('product: ', product);
-
-console.log(productCount * productPrice);
-
-console.log(`На складе ${productCount} единиц(ы) товара ${product} на сумму ${productCount * productPrice} рублей`);
 }
 
 {
 const product = 'Часы';
 const productCount = 5;
-const productCategory = 'Акссесуары';
+const productCategory = 'Аксесcуары';
 const productPrice = 10299;
-
-//console.log('product: ',product);
-//console.log(productCount * productPrice);
 }
 
 {
   const product = prompt('Наименование товара');
-  const productCount = Number(prompt('Количество товара'));  
+  const productCount = +prompt('Количество товара');  
   const productCategory = prompt('Категория товара');
   const productPrice = +prompt('Цена товара');
-  
-  console.log('Наименование товара: ', product);
-  console.log('Количество товара: ', productCount);
-  console.log('Категория товара: ', productCategory);
-  console.log('Цена товара: ', productPrice);
-  
-  console.log(typeof product);
-  console.log(typeof productCount);
-  console.log(typeof productCategory);
-  console.log(typeof productPrice);  
+
+  if (!productCount) {    
+    console.log('Количество товара:', 'Вы ввели некорректные данные!');
+  }
+
+  if (!productPrice) {    
+    console.log('Цена товара:', 'Вы ввели некорректные данные!');
+  }
 }
 
 
