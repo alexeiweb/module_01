9253f95e88c7c98c3b6f86694bfe6c8be4d1333a
Alexei Web home <alexeiiweb@gmail.com>
2021-12-22 22:02:39 +0300

    Add Lesson 4-4

--- js/tasks.js
+++ js/tasks.js
@@ -1,13 +1,13 @@
 'use strict'
 {
-const rain = Math.round(Math.random());
-console.log(rain);
+  const rain = Math.round(Math.random());
+  console.log(rain);
 
-if (rain === 0) {
-  console.log('Дождя нет!');  
-} else {
-  console.log('Пошёл дождь. Возьмите зонт!');
-}
+  if (rain === 0) {
+    console.log('Дождя нет!');  
+  } else {
+    console.log('Пошёл дождь. Возьмите зонт!');
+  }
 }
 
 {
@@ -16,6 +16,13 @@ if (rain === 0) {
   const IT = +prompt('Введите кол-во баллов по информатике:');
 
   const result = (((math + russian + IT) >= 265) ? 'Поздравляю, Вы поступили на бюджет!' : 'Попробуйте в следующем году');
+  console.log(result);
+}
+
+{
+  const money = +prompt('Укажите сумму, которую Вы хотите снять?');
+
+  const result = ((Number.isInteger(money / 100)) ? 'Вы можете снять указанную сумму' : 'Вы не можете снять указанную сумму');
   console.log(result);
 }
 
