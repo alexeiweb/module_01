fed29a2cc2d2f622e9f86a596af5d2b8cfff3340
Alexei Web <alexeiiweb@gmail.com>
2021-12-24 16:44:13 +0300

    Add Lesson 5

---
+++ Lesson_05/js/task01.js
@@ -0,0 +1,51 @@
+'use strict'
+
+// let cash = +prompt('Введите сумму:');
+// const euro = 1.2;
+// const dollar = 73;
+
+// const change = () => {
+//   rouble = (cash * euro * dollar);
+//   console.log('rouble: ', cash);
+// };
+
+/////////////////////////////////////
+
+// const convertDollars = (cash) => {
+//   cash * 1.2;
+//   return 'доллары';
+// };
+
+// const convertRoubles = (cash) => {
+//   cash * 73;
+//   return 'рубли';
+// };
+
+// const payProduct = (product) => {
+//   console.log('заплатили евро');
+
+//   console.log('заплатили доллары');
+
+//   const roubles = convertRoubles(product);
+//   console.log('product: ', product);
+
+//   return 'сумма в рублях';
+// }
+
+// const result = payProduct(23);
+// console.log('result: ', result);
+
+///////////////////////////////
+const x = 1;
+const y = 2;
+
+const pay = (x, y) => {
+  const res = (x + y);
+  return res;
+  console.log(res);
+};
+
+
+
+
+
